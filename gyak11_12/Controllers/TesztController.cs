31a92fbcd33516bd79afaa647a9326210781c75e
Zsófia Tóth <Zsfia Tth@LAPTOP-ZSOZSESZ>
2022-11-30 21:57:05 +0100

    Add project files.

---
+++ gyak11_12/Controllers/TesztController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gyak11_12.Controllers
+{
+ //   [Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = pontosIdő
+            };
+        }
+    }
+}
