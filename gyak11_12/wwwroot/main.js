87ac13fac397c593f4a4852bd0247334981b0f07
Máté Tóth <mate.toth9@stud.uni-corvinus.hu>
2022-12-07 12:21:44 +0100

    tovább

---
+++ gyak11_12/wwwroot/main.js
@@ -0,0 +1,64 @@
+﻿window.onload = function (e) {
+    console.log("Oldal betöltve...");
+    document.getElementById("elore_gomb").onclick = előre;
+    document.getElementById("vissza_gomb").onclick = vissza;
+    kérdésBetöltés(questionId)
+}
+    var jóVálasz
+    var questionId = 1
+
+    
+    function előre() {
+        questionId++
+        kérdésBetöltés(questionId)
+    }
+
+    function vissza() {
+        questionId--
+        kérdésBetöltés(questionId)
+    }
+
+    function kérdésMegjelenítés(kerdes) {
+        if (!kerdes) return
+        console.log(kerdes);
+        document.getElementById("kérdés_szöveg").innerText = kerdes.questionText
+        document.getElementById("válasz1").innerText = kerdes.answer1
+        document.getElementById("válasz2").innerText = kerdes.answer2
+        document.getElementById("válasz3").innerText = kerdes.answer3
+        jóVálasz = kerdes.correctAnswer
+        if (kérdés.image) {
+            document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+            document.getElementById("kép").classList.remove("rejtett")
+        }
+        else {
+            document.getElementById("kép").classList.add("rejtett")
+        }
+        document.getElementById("válasz1").classList.remove("jo", "rossz");
+        document.getElementById("válasz2").classList.remove("jo", "rossz");
+        document.getElementById("válasz3").classList.remove("jo", "rossz");
+    }
+
+    function kérdésBetöltés(id) {
+        fetch(`/questions/${id}`)
+            .then(válaszfeldolgozás)
+            .then(kérdésMegjelenítés);
+    }
+
+    function válaszfeldolgozás(válasz) {
+        if (!válasz.ok) {
+            console.error(`Hibás válasz: ${response.status}`)
+        }
+        else {
+            return válasz.json()
+        }
+}
+
+function választás(n) {
+    if (n != jóVálasz) {
+        document.getElementById(`válasz${n}`).classList.add("rossz");
+        document.getElementById(`válasz${jóVálasz}`).classList.add("jo");
+    }
+    else {
+        document.getElementById(`válasz${jóVálasz}`).classList.add("jo");
+    }
+}
