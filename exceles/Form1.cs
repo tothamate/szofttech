5524ae6daf5fd72f7b31b85f5766fce0392b5d04
Máté Tóth <mate.toth9@stud.uni-corvinus.hu>
2022-12-07 19:37:23 +0100

    15. feladatig

--- exceles/Form1.cs
+++ exceles/Form1.cs
@@ -1,10 +1,89 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
 namespace exceles
 {
     public partial class Form1 : Form
     {
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB;     // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
         public Form1()
         {
             InitializeComponent();
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        void CreateTable()
+        {
+            string[] fejlécek = new string[] {
+                "Kérdés",
+                "1. válasz",
+                "2. válaszl",
+                "3. válasz",
+                "Helyes válasz",
+                "kép"};
+
+            for (int i = 0; i < fejlécek.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = fejlécek[i];
+            }
+
+            gyak11_12.Models.HajosContext hajosContext = new gyak11_12.Models.HajosContext();
+            var mindenKérdés = hajosContext.Questions.ToList();
+
+            object[,] adatTömb = new object[mindenKérdés.Count(), fejlécek.Count()];
+
+            for (int i = 0; i < mindenKérdés.Count(); i++)
+            {
+                adatTömb[i, 0] = mindenKérdés[i].Question1;
+                adatTömb[i, 1] = mindenKérdés[i].Answer1;
+                adatTömb[i, 2] = mindenKérdés[i].Answer2;
+                adatTömb[i, 3] = mindenKérdés[i].Answer3;
+                adatTömb[i, 4] = mindenKérdés[i].CorrectAnswer;
+                adatTömb[i, 5] = mindenKérdés[i].Image;
+            }
+
+            int sorokSzáma = adatTömb.GetLength(0);
+            int oszlopokSzáma = adatTömb.GetLength(1);
+
+            Excel.Range adatRange = xlSheet.get_Range("A2", Type.Missing).get_Resize(sorokSzáma, oszlopokSzáma);
+            adatRange.Value2 = adatTömb;
+
+            adatRange.Columns.AutoFit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
